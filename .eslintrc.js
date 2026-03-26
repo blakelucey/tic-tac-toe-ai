5eb8df16162bea6e171f61eb351db708d0872c84
Blake M Lucey <blucey7@gmail.com>
2026-03-25 21:44:46 -0500

    feat: initialize repo

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    'expo',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+## Getting Started
+
+### Using Bun (recommended)
+bun install
+bun run start:bun
+
+### Using npm (fallback)
+npm install
+npx expo start
